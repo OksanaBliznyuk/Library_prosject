f83ea16db49e16acbde11edb0da409a488a0068e
OksanaBliznyuk <okstana@atticum.no>
2022-09-27 13:23:11 +0200

    first commit

---
+++ library.js
@@ -0,0 +1,721 @@
+const booklist = [
+  {
+    id: ["0"],
+    title: "A guide to the Prosject Management Body of Knowlege (PMBOK guid)",
+    isbn: "9781628251845",
+    year: "2017",
+    edition: "6",
+
+    studyLine: ["Prosjektledelse"],
+
+    authorName: "",
+  },
+  {
+    id: ["0"],
+    title:
+      "A guide to the Project Management Body of Knowledge (PMBOK guid) and the Standard for project management",
+    isbn: "9781628256642",
+    year: "2021",
+    edition: "7",
+
+    studyLine: ["Prosjektledelse"],
+
+    authorName: "",
+  },
+  {
+    id: ["0"],
+    title:
+      "An Investigation Into the effectiveness of Banner Advertising on different social media: Banner Advertisement",
+    isbn: "9781544055282",
+    year: "2017",
+    edition: "Createspace Independent",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName: "Eric Kwaku Donkor, ",
+  },
+
+  {
+    id: ["0"],
+    title: "Beginning Programming with Java for Dummies",
+    isbn: "978-1119235538",
+    year: "",
+    edition: "5",
+
+    studyLine: ["Backend - programmering"],
+
+    authorName: "",
+  },
+  {
+    id: ["0"],
+    title:
+      "Byg med fra start; Lær webkodning og udvikl en færdig hjemmeside på 3 dage; HTML </> CSS",
+    isbn: "9788797132203",
+    year: "2019",
+    edition: "",
+
+    studyLine: [],
+
+    authorName: "Frandsen, E. & Spangsdorf, S.",
+  },
+  {
+    id: ["1"],
+    title: "Coaching",
+    isbn: "9788215022338",
+    year: "2019",
+    edition: "",
+
+    studyLine: ["Salg, ledelse og markedsføring"],
+
+    authorName: "Berg og Ribe",
+  },
+  {
+    id: ["1", "2", "3", "4"],
+    title:
+      "Content Marketing - The complete guide for beginners to learn content marketing and strategies",
+    isbn: "9781704867038",
+    year: "2019",
+    edition: "Independently Published",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "David Miller",
+  },
+
+  {
+    id: ["1", "2"],
+    title: "Digital Markedsføring og kommunikation",
+    isbn: "978-82-02-70031-7",
+    year: "2021",
+    edition: "second",
+
+    studyLine: ["Salg og ledelse", "prosjektledelse", "digital markedsforing"],
+
+    authorName: "Nina Furu ",
+  },
+  {
+    id: ["0"],
+    title: "Digital Marketing Strategy",
+    isbn: "9780749484224",
+    year: "2019",
+    edition: "second",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName: "Simon Kingsnorth",
+  },
+  {
+    id: ["0"],
+    title: "Dont make me think",
+    isbn: ["978-0-321-96551-6", "0-321-96551-6"],
+    year: "2014",
+    edition: "Wiley",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName: "Steve Krug",
+  },
+  {
+    id: ["1"],
+    title:
+      "Email Marketing Demystified: Build a Massive Mailing List, Write Copy that Converts and Generate More Sales",
+    isbn: "978-0-990-5300-1-5",
+    year: "2015",
+    edition: "Consumer News LLC",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName: "Matthew Paulson",
+  },
+  {
+    id: ["1", "2", "3", "4", "5"],
+    title: "Etikk og bærekraft",
+    isbn: "9788215043074",
+    year: "2020",
+    edition: "Universitetsforlaget",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: " Øyvind Kvalnes",
+  },
+  {
+    id: ["1"],
+    title: "Forbrukeratferd",
+    isbn: "9788205536906",
+    year: "2020",
+    edition: "Gyldendal, 1",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Fagerstrøm, Eg, Johannessen & Vogt",
+  },
+  {
+    id: ["0"],
+    title: "Forbrukeratferd",
+    isbn: "9788205558748",
+    year: "2022",
+    edition: "Gyldendal, 2",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Fagerstrøm, Eg, Johannessen & Vogt",
+  },
+  {
+    id: ["0"],
+    title: "Frontend Architecture for Design Systems",
+    isbn: " 9781491926789",
+    year: "2019",
+    edition: "Heftet, Engelsk,",
+
+    studyLine: ["Frontend- utvikling "],
+
+    authorName: " Micah Godbolt",
+  },
+  {
+    id: ["0"],
+    title: "Grunnleggende regnskap",
+    isbn: "9788205510784",
+    year: "2020",
+    edition: "third",
+
+    studyLine: ["Økonomi"],
+
+    authorName: "Kjell Magne Basksaas",
+  },
+  {
+    id: ["1", "2", "3"],
+    title: "Grønn markedsføringsledelse",
+    isbn: "9788245024289",
+    year: "2019",
+    edition: "Vigmostad & Bjørke AS",
+
+    studyLine: ["Digital Markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Arne Nygaard",
+  },
+  {
+    id: ["0"],
+    title: "Hvorfor kjøpte pappa porsche? En innføring i forbrukeratferd",
+    isbn: " 9788205534421",
+    year: "2021",
+    edition: "?",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Njøten, H",
+  },
+
+  {
+    id: ["1", "2", "3"],
+    title: "Inbound marketing",
+    isbn: "978-93-87284-52-4",
+    year: "2018",
+    edition: "BPB Publications",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: [
+      "Dr. Chandrani Singh",
+      "Dr. Abiresh Abraham",
+      "Mr. Ajit Pandey",
+    ],
+  },
+  {
+    id: ["1"],
+    title: "inbound marketing attract, engage, and delight customers online",
+    isbn: [
+      "978-1-118-89665-5(pbk)",
+      "978-1-118-89659-4(ebk)",
+      "978-1-118-89670-9(ebk)",
+    ],
+    year: "2014",
+    edition: "?",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: ["Brian Halligan", "Dharmesh Shah"],
+  },
+  {
+    id: ["1"],
+    title: "Innholdsmarkedsføring Fortjent, eid og kjøp",
+    isbn: "978-82-05-53514-5",
+    year: "2020",
+    edition: "second",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Pål Nisja-Wilhelmsen",
+  },
+  {
+    id: ["0"],
+    title: "Introduction to programmatic advertising",
+    isbn: "978-80-260-9611-5",
+    year: "2016",
+    edition: "Adtechresearch",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName: "Dominik Kosorin",
+  },
+  {
+    id: ["1", "2", "3", "4", "5"],
+    title: "Jus for økonomiske og administrative studier",
+    isbn: "9788202563776",
+    year: "2017",
+    edition: "Cappelen Damm",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Geir Heivoll & Henrik Sander",
+  },
+  {
+    id: ["1", "2", "3", "4", "5", "6"],
+    title: "Key Account Management",
+    isbn: "978-82-15-00926-1",
+    year: "2022",
+    edition: "third",
+
+    studyLine: ["Salg, ledelse og markedsføring "],
+
+    authorName: "Eirik Petersen",
+  },
+  {
+    id: ["0"],
+    title: "Kom først i mål: Hvordan skrive vinnende tilbud ",
+    isbn: "9788275362016",
+    year: "2021",
+    edition: "?",
+
+    studyLine: ["Salg, ledelse og markedsføring "],
+
+    authorName: "Kunøe, G",
+  },
+  {
+    id: ["0"],
+    title: "Kundeorientering og CRM",
+    isbn: "9788275362023",
+    year: "2022",
+    edition: "",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Kunøe, G",
+  },
+  {
+    id: ["1", "2"],
+    title: "Learning Google AdWords and Google Analytics",
+    isbn: [
+      "978-0-9943904-4-8",
+      "978-0-9943904-5-5(eBook)",
+      "978-0-9943904-6-2(eBook)",
+      "978-0-9943904-7-9(eBook)",
+    ],
+    year: "2018",
+    edition: "second",
+
+    studyLine: ["Digital  markedsføring"],
+
+    authorName: "Benjamin Mangold",
+  },
+  {
+    id: ["1", "2", "3", "4", "5", "6", "7"],
+    title: "Lov og rett for næringslivet",
+    isbn: "?",
+    year: "2022",
+    edition: "Twenty nine",
+
+    studyLine: ["Digital  markedsføring"],
+
+    authorName:
+      " Sverre Faafeng langfeldt, Tore Bråten, Monica Viken & Stine Winger Minde",
+  },
+  {
+    id: ["0"],
+    title: "Managing Successful Projects with PRINCE2",
+    isbn: "9780113315338",
+    year: "2017",
+    edition: "Axelos",
+
+    studyLine: ["Proskjektledelse "],
+
+    authorName: "?",
+  },
+  {
+    id: ["0"],
+    title: "Markedsføring og kommunikasjon på nett",
+    isbn: "9788202512347",
+    year: "2017",
+    edition: "First",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName: "Nina Furu",
+  },
+  {
+    id: ["0"],
+    title: "Markedsføringsledelse ",
+    isbn: "9788205472808",
+    year: "2016",
+    edition: "Akademia",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Keller, Kotler & Lane",
+  },
+  {
+    id: ["0"],
+    title: "Mellomlederen ",
+    isbn: "",
+    year: "2015",
+    edition: "?",
+
+    studyLine: [],
+
+    authorName: "Ole Hope",
+  },
+  {
+    id: ["0"],
+    title:
+      "Muntlig formidling; Hvirdan nå frem med ditt budskap i små og store forsamlinger",
+    isbn: "9788215030821",
+    year: "2019",
+    edition: "?",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "?",
+  },
+  {
+    id: ["0"],
+    title:
+      "Nytt perpektiv på organisasjon og ledelse, struktur, HR, politikk og symboler",
+    isbn: " 9788205507678",
+    year: "2020",
+    edition: "sixth",
+
+    studyLine: ["Salg, ledelse og markedsføring"],
+
+    authorName: "Bolman, L., G, & Deal, T., E.",
+  },
+  {
+    id: ["1"],
+    title: "Nå dine salgsmål",
+    isbn: "9788215027814",
+    year: "2016",
+    edition: "Universitetsforlaget",
+
+    studyLine: ["Salg, ledelse og markedsføring"],
+
+    authorName: "Eirik Petersen",
+  },
+  {
+    id: ["0"],
+    title: "Orginasasjon og ledese",
+    isbn: "9788205481886",
+    year: "2019",
+    edition: "?",
+
+    studyLine: ["Salg, ledelse og markedsføring"],
+
+    authorName: "Valvatne Einarsen, S., Martinsen, Ø., L. & Skogstad, A.",
+  },
+  {
+    id: ["0"],
+    title: "Personvern og GDPR i praksis",
+    isbn: "9788202604066",
+    year: "2022",
+    edition: "?",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Jarbekk, E. & Sommerfeldt, S.",
+  },
+  {
+    id: ["0"],
+    title: "Projekthåndboka 3.0 -Verktøykasse for kreative team",
+    isbn: "9788215027555",
+    year: "2016",
+    edition: "?",
+
+    studyLine: ["Proskjektledelse"],
+
+    authorName: "Aakre & Scharning",
+  },
+  {
+    id: ["0"],
+    title: "Prosjektarbeid; Utviklings- og endringskompetanse",
+    isbn: "9788205383616",
+    year: "2021",
+    edition: "sixth",
+
+    studyLine: ["Proskjektledelse"],
+
+    authorName: "Harald Westhagen",
+  },
+
+  {
+    id: ["0"],
+    title: "Prosjektledelse; En veiledning for studenter",
+    isbn: "9788245033731",
+    year: "2020",
+    edition: "fifth",
+
+    studyLine: ["Proskjektledelse"],
+
+    authorName: "Erling S. Andersen & Eva Schwencke",
+  },
+  {
+    id: ["0"],
+    title: "Prosjektledelse; Fra initiering til gevinstrealisering",
+    isbn: "9788215043050",
+    year: "2021",
+    edition: "fifth",
+
+    studyLine: ["Proskjektledelse"],
+
+    authorName: "Jan Terje Karlsen",
+  },
+  {
+    id: ["0"],
+    title: "Salg og salgledelse",
+    isbn: "9788275360432",
+    year: "2017",
+    edition: "?",
+
+    studyLine: ["Salg, ledelse og marketing"],
+
+    authorName: "Gorm Kunøe, Kompendiet",
+  },
+  {
+    id: ["1", "2", "3", "4", "5", "6", "7"],
+    title: "",
+    isbn: "",
+    year: "",
+    edition: "",
+
+    studyLine: [],
+
+    authorName: "",
+  },
+  {
+    id: ["1"],
+    title: "Salgets helter",
+    isbn: "9788215024448",
+    year: "2016",
+    edition: "Universitetsforlaget",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Christian Hofbauer",
+  },
+  {
+    id: ["1", "2"],
+    title:
+      "SEO 2020 Learn Search Engine Optimization With Smart Internet Marketing Strategies",
+    isbn: "9781730775758",
+    year: "2020",
+    edition: "Simple Effectivenes Publishing",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Adam Clarke",
+  },
+  {
+    id: ["0"],
+    title: "Service, påvirkning, personlig salg, profesjonsetikk",
+    isbn: "9788275360432",
+    year: "2014",
+    edition: "?",
+
+    studyLine: ["Salg, ledelse og markedsføring "],
+
+    authorName: "Gorm Kunøe",
+  },
+  {
+    id: ["0"],
+    title: "Skriv så det selger",
+    isbn: "9788215027227",
+    year: "2016",
+    edition: "Universitetsforlaget,",
+
+    studyLine: ["Salg, ledelse og markedsføring "],
+
+    authorName: "Christine Caalvert",
+  },
+
+  {
+    id: ["1"],
+    title: "Små og store data",
+    isbn: "9788205512719",
+    year: "2019",
+    edition: "?",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName: "Pål Nisja, Wilhelmesen",
+  },
+  {
+    id: ["0"],
+    title:
+      "Social Media Marketing Workbook: How to use Social Media for Business-",
+    isbn: "9781539598145",
+    year: "2021",
+    edition: "?",
+
+    studyLine: ["Digital markedsføring "],
+
+    authorName:
+      "Jason McDonald, JM Inernational Group and Excerpti Communications",
+  },
+  {
+    id: ["0"],
+    title:
+      "Social Media Marketing Workbook: How to use Social Media for Business",
+    isbn: "9798839797161",
+    year: "2022",
+    edition: "?",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName:
+      "Jason McDonald, JM Inernational Group and Excerpti Communications",
+  },
+  {
+    id: ["0"],
+    title: "Strategic Social Media Managment: Theory And Practice",
+    isbn: "9789811546570",
+    year: "2021",
+    edition: "?",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName: "Karen E. Sutherland",
+  },
+  {
+    id: ["0"],
+    title:
+      "The retargeting playbook; How to turn Web-window shoppers into customers",
+    isbn: "9781118832646",
+    year: "2014",
+    edition: "?",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName: "Berke, Fulton & Vaccarello",
+  },
+  {
+    id: ["0"],
+    title: "The website in html and css",
+    isbn: "9788797132210",
+    year: "2019",
+    edition: "?",
+
+    studyLine: ["Frontend- utvikling "],
+
+    authorName: "Emil M. Frandsen og Steeven, Spangsdorf",
+  },
+  {
+    id: ["0"],
+    title:
+      "Understanding Digital Marketing: A complete Guide to Engaging customers and implementing successful didital campaigns",
+    isbn: "781789666014",
+    year: "2021",
+    edition: "fifth",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Damian Ryna",
+  },
+  {
+    id: ["1"],
+    title:
+      "Understanding Digital Marketing: Marketing Strategies for Engaging the Digital Generation",
+    isbn: "9780749478438",
+    year: "2016",
+    edition: "?",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName: "Damian Ryna & Kogan Page Ltd",
+  },
+  {
+    id: ["0"],
+    title: "Universell utforming av IKT-systemerbrukergrensesnitt for alle",
+    isbn: "9788215030333",
+    year: "2018",
+    edition: "?",
+
+    studyLine: ["Frontend-utvikling"],
+
+    authorName: "Frode Eika Sandnes",
+  },
+  {
+    id: ["0"],
+    title: "Verdiskapende prosjektledelse",
+    isbn: "7882026299458",
+    year: "2022",
+    edition: "second",
+
+    studyLine: ["Prosjectledelse"],
+
+    authorName: "Torgeir Skyttermoen & Annne Live Vaagaasar",
+  },
+  {
+    id: ["1"],
+    title:
+      "WordPress for Beginners 2021: A Visual Step-by-Step Guide to Mastering WordPress",
+    isbn: "9798584887780",
+    year: "2020",
+    edition: "?",
+
+    studyLine: ["Digital markedsføring"],
+
+    authorName: "Dr. Andy Williams",
+  },
+  {
+    id: ["1"],
+    title: "Økonomistyring for LØM-emnet",
+    isbn: "9788245016444",
+    year: "2014",
+    edition: "Fagbokforlaget",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Hjertnes og Sending",
+  },
+  {
+    id: ["0"],
+    title: "Økonomistyring for LØM-emnet ",
+    isbn: "9788245024678",
+    year: "2018",
+    edition: "Fagbokforlaget",
+
+    studyLine: ["Digital markedsføring + Salg, ledelse og markedsføring"],
+
+    authorName: "Hjertnes og Sending",
+  },
+  {
+    id: ["0"],
+    title: "Økonomistyring: innføring i bedriftsøkonomi og regnskap",
+    isbn: "9788205446854",
+    year: "2015",
+    edition: "?",
+
+    studyLine: ["Økonomi"],
+
+    authorName: "Kjell Magne Baksaas,",
+  },
+  {
+    id: ["1", "2", "3"],
+    title: "Banner Advertisement",
+    isbn: ["1544055285", "978-1544055282"],
+    year: "2020",
+    edition: "first",
+
+    studyLine: ["salg og ledelse", "prosjektledelse", "digital markedsforing?"],
+
+    authorName: "Eric Kwaku Donkor",
+  },
+];
+
+export default booklist;
